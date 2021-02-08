afb530ea25ce112986e2fd86c9b35cd5d4b2fcf6
Harsh Vardhan Guleria <harshvardhanguleria@gmail.com>
2021-02-08 23:58:46 +0530

    Modified

--- git_push_auto.py
+++ git_push_auto.py
@@ -1,4 +1,12 @@
 import os
-os.system('cmd /k "git add -A"')
-os.system('cmd /k "git commit -m "Modified"')
-os.system('cmd /k "git push"')
+i = 1
+while (i == 3):
+    if (i == 1):
+        os.system('cmd /k "git add -A"')
+        i = i + 1
+    elif (i == 2):
+        os.system('cmd /k "git commit -m "Modified"')
+        i = i + 1
+    elif (i == 3):
+        os.system('cmd /k "git push"')
+        i = i + 1
