9a5b94420a1f2a16753d96157d98fcf5e2671853
Harsh Vardhan Guleria <harshvardhanguleria@gmail.com>
2021-07-25 14:48:39 +0530

    Modified

--- SLL.py
+++ SLL.py
@@ -2,7 +2,7 @@
 
 # Node class
 class Node:
-    def __init__(self, data):
+    def __init__(self, data=None):
         self.data = data
         self.next = None
 
@@ -11,16 +11,22 @@ class SLL:
     def __init__(self):
         self.head = None
 
-    def insert(self, newNode):
+    def insertEnd(self, newNode):
         if self.head is None:
             self.head = newNode
         else:
             lastNode = self.head
             while lastNode.next is not None:
-                #if lastNode.next is None:
-                #    break
                 lastNode = lastNode.next
             lastNode.next = newNode
+    
+    def insertBeginnig(self, newNode):
+        if self.head is None:
+            self.head = newNode
+        else:
+            firstNode = newNode
+            firstNode.next = self.head
+            self.head = firstNode
     
     def printList(self):
         if self.head is None:
@@ -37,9 +43,11 @@ class SLL:
 # Creating a node
 firstNode = Node("Harsh")
 linkedList = SLL()
-linkedList.insert(firstNode)
+linkedList.insertEnd(firstNode)
 secondNode = Node("Ben")
-linkedList.insert(secondNode)
+linkedList.insertEnd(secondNode)
 thirdNode = Node("Mathew")
-linkedList.insert(thirdNode)
+linkedList.insertEnd(thirdNode)
+fourthNode = Node("Anna")
+linkedList.insertBeginnig(fourthNode)
 linkedList.printList()
