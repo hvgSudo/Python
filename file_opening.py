22b37b4baf1393b7e3091eca0d2fb492b7f24f90
Harsh Vardhan Guleria <harshvardhanguleria@gmail.com>
2021-08-28 11:53:58 +0530

    Modified

--- file_opening.py
+++ file_opening.py
@@ -7,20 +7,20 @@ for line in fhandle :
 #    for i in file:
 #        print(i.strip())
 
-print("Breast Cancer.names")
-with open("/mnt/d/breast-cancer.names") as f:
-    c = 0
-    for i in f:
+#print("Breast Cancer.names")
+#with open("/mnt/d/breast-cancer.names") as f:
+#    c = 0
+#    for i in f:
         #if c == 5:
         #    break
-        print(i)
-        c = c + 1
+#        print(i)
+#        c = c + 1
 
 print("Breast Cancer.data")
 with open("/mnt/d/breast-cancer.data") as file:
     count = 0
     for i in file:
-        if count == 5:
-            break
+        #if count == 5:
+        #    break
         print(i)
-        count = count + 1
+        #count = count + 1
