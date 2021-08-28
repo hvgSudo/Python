1ba9e2d99c85b43572ba301a7f7f9b4b2ec157be
Harsh Vardhan Guleria <harshvardhanguleria@gmail.com>
2021-08-28 11:03:10 +0530

    Modified

--- file_opening.py
+++ file_opening.py
@@ -3,6 +3,24 @@ fhandle = open(fname)
 for line in fhandle :
     line = line.rstrip()
     print(line)'''
-with open("Harsh.txt") as file:
+# with open("Harsh.txt") as file:
+#    for i in file:
+#        print(i.strip())
+
+print("Adult.names")
+with open("/mnt/d/adult.names") as f:
+    c = 0
+    for i in f:
+        #if c == 5:
+        #    break
+        print(i)
+        c = c + 1
+
+print("Adult.data")
+with open("/mnt/d/adult.data") as file:
+    count = 0
     for i in file:
-        print(i.strip())
+        if count == 5:
+            break
+        print(i)
+        count = count + 1
