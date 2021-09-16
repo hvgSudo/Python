ff69c28f50b15e03a73afe481a089d93887d48b8
harshvardhanguleria <harshvardhanguleria@gmail.com>
2021-09-16 22:32:40 +0530

    Modified

---
+++ sql_data_insert.py
@@ -0,0 +1,26 @@
+import xlrd
+import mysql.connector
+book = xlrd.open_workbook("/home/kingston-machismo/Documents/mySQL.xls")
+sheet = book.sheet_by_name("Sheet1")
+
+database = mysql.connector.connect(
+    host="localhost",
+    user="kingston-machismo",
+    passwd="AbcD123#",
+    database="dbms"
+)
+
+cursor = database.cursor()
+query = """INSERT into Publishers (Pid, Pname) VALUES (%d, %s)"""
+
+for r in range(1, sheet.nrows):
+    Pid = sheet.cell(r, 0).value
+    Pname = sheet.cell(r, 1).value
+
+    values = (int(Pid), Pname)
+    cursor.execute(query, values)
+
+cursor.close()
+database.commit()
+database.close()
+print("Data entered")
