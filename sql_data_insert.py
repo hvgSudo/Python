914ea89a80957d36c448762d550ffd103ec6cf70
Harsh Vardhan Guleria <harshvardhanguleria@gmail.com>
2021-09-20 21:40:37 +0530

    Modified

--- sql_data_insert.py
+++ sql_data_insert.py
@@ -1,17 +1,33 @@
 import mysql.connector as m
-import pandas as pd
+import xlrd
 
 mydb = m.connect(host="localhost", user="kingston-machismo", passwd="AbcD123#", database="dbms")
 
 mycursor = mydb.cursor()
 
-# mycursor.execute("create table SIULibrary(Slid int primary key, Lname varchar(30) not null, Location varchar(20) not null, Noofbranches int default '1')")
+l=list()
+loc = ("/mnt/d/sql_input_data.xls")
+a = xlrd.open_workbook(loc)
+sheet = a.sheet_by_index(0)
+sheet.cell_value(0, 0)
+for i in range(1, 11):
+    l.append(tuple(sheet.row_values(i)))
+
+q = "insert into ILibrary(Lid, Lname, city, Area, Slid) values (%s, %s, %s, %s, %s)"
+mycursor.executemany(q, l)
+mydb.commit()
+mydb.close()
+
+'''
+import pandas as pd
 query = "insert into SIULibrary(Slid, Lname, Location, Noofbranches) values(%d, %s, %s, %d)"
-inputData = pd.read_excel("D:\\sql_input_data.xlsx")
+inputData = pd.read_excel("/mnt/d/sql_input_data.xlsx")
 data = inputData.values.tolist()
-tup = tuple()
 lst = list()
 for i in data:
     lst.append(tuple(i))
+query = "insert into ILibrary(Lid, Lname, city, Area, Slid) values (%s, %s, %s, %s, %s)"
 mycursor.executemany(query, lst)
-# mycursor.executemany("select * from flights")
+mydb.commit()
+mydb.close()
+'''
