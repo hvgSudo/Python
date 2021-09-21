65f97926c6cba2fcc9834334185ddbe400030595
Harsh Vardhan Guleria <harshvardhanguleria@gmail.com>
2021-09-22 00:04:19 +0530

    Modified

--- sql_data_insert.py
+++ sql_data_insert.py
@@ -1,11 +1,34 @@
+# This script works well on debugger but it faces segmentation error while executing
+# via terminal.
+# This script was developed using VS Code and its run and debug function proved 
+# beneficial to run it.
 import mysql.connector as m
-#import xlrd
 
 mydb = m.connect(host="localhost", user="kingston-machismo", passwd="AbcD123#", database="dbms")
 
 mycursor = mydb.cursor()
 
-'''l=list()
+# Method 1 using pandas 
+
+import pandas as pd
+# query = "insert into SIULibrary(Slid, Lname, Location, Noofbranches) values(%d, %s, %s, %d)"
+# mycursor.execute(query)
+inputData = pd.read_excel("/mnt/d/sql_input_data.xlsx")
+data = inputData.values.tolist() 
+lst = list() 
+for i in data:
+    lst.append(tuple(i))
+query = "insert into employee(Empid, Empname, Email, Salary, Lid) values (%s, %s, %s, %s, %s)"
+mycursor.executemany(query, lst)
+mydb.commit()
+mydb.close()
+
+
+# Method 2 using xlrd
+
+'''
+import xlrd
+l=list()
 loc = ("/mnt/d/sql_input_data.xls")
 a = xlrd.open_workbook(loc)
 sheet = a.sheet_by_index(0)
@@ -17,17 +40,4 @@ q = "insert into ILibrary(Lid, Lname, city, Area, Slid) values (%s, %s, %s, %s, 
 mycursor.executemany(q, l)
 mydb.commit()
 mydb.close()
-
 '''
-import pandas as pd
-#query = "insert into SIULibrary(Slid, Lname, Location, Noofbranches) values(%d, %s, %s, %d)"
-inputData = pd.read_excel("/mnt/d/sql_input_data.xlsx")
-data = inputData.values.tolist() 
-lst = list() 
-for i in data:
-    lst.append(tuple(i))
-query = "insert into employee(Empid, Empname, Email, Salary, Lid) values (%s, %s, %s, %s, %s)"
-mycursor.executemany(query, lst)
-mydb.commit()
-mydb.close()
-# '''
